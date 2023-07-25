0b1bc69138d9665cdf9cb648d0f5f33bfd477bc0
Kiran Anand <liteninkiran@hotmail.com>
2023-07-25 18:30:51 +0100

    28 - Protecting contact routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,7 +3,7 @@ const Contact = require("../models/contactModel");
 
 // @desc Get all contacts
 // @route GET /api/contacts
-// @access public
+// @access private
 const getContacts = asyncHandler(async (req, res) => {
     const contacts = await Contact.find();
     res.status(200).json(contacts);
@@ -11,7 +11,7 @@ const getContacts = asyncHandler(async (req, res) => {
 
 // @desc Create a contact
 // @route POST /api/contacts
-// @access public
+// @access private
 const createContact = asyncHandler(async (req, res) => {
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
@@ -24,7 +24,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 // @desc Get a contact
 // @route GET /api/contacts/:id
-// @access public
+// @access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -36,7 +36,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 // @desc Update a contact
 // @route PUT /api/contacts/:id
-// @access public
+// @access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -53,7 +53,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 // @desc Delete a contact
 // @route DELETE /api/contacts/:id
-// @access public
+// @access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
