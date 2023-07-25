1636c8b8aa2259d249af39ad78d434bbe3d92ed1
Kiran Anand <liteninkiran@hotmail.com>
2023-07-25 16:46:08 +0100

    18 - CRUD update contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -38,7 +38,17 @@ const getContact = asyncHandler(async (req, res) => {
 // @route PUT /api/contacts/:id
 // @access public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error(`Contact ${req.params.id} not found`);
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.status(200).json(updatedContact);
 });
 
 // @desc Delete a contact
