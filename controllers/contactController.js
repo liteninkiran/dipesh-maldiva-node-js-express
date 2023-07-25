e24d635419798fce3a084643a89fc2da672b1595
Kiran Anand <liteninkiran@hotmail.com>
2023-07-25 16:39:26 +0100

    16 - CRUD get all contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,23 +1,25 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require("../models/contactModel");
 
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
 const getContacts = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get all contacts'});
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
 });
 
 // @desc Create a contact
 // @route POST /api/contacts
 // @access public
 const createContact = asyncHandler(async (req, res) => {
-    console.log(req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         res.status(400);
         throw new Error('All fields are mandatory');
     }
-    res.status(201).json({ message: 'Create a contact'});
+    const contact = await Contact.create({ name, email, phone });
+    res.status(201).json(contact);
 });
 
 // @desc Get a contact
