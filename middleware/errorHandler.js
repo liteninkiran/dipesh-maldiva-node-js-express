58ef885b46b7c1ad9cf7eb79dc169c0c338717ef
Kiran Anand <liteninkiran@hotmail.com>
2023-07-25 16:12:32 +0100

    11 - Error handling middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,24 @@
+const { constants } = require("../constants");
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    const object = {
+        title: null,
+        message: err.message,
+        stackTrace: err.stack,
+    };
+
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR : object.title = 'Validation Failed' ; break;
+        case constants.NOT_FOUND        : object.title = 'Not Found'         ; break;
+        case constants.UNAUTHORIZED     : object.title = 'Unauthorized'      ; break;
+        case constants.FORBIDDEN        : object.title = 'Forbidden'         ; break;
+        case constants.SERVER_ERROR     : object.title = 'Server Error'      ; break;
+        default                         : console.log("No Error, All good !"); break;
+    }
+
+    if (object.title) {
+        res.json(object);
+    }
+};
+
+module.exports = errorHandler;
